15168e172c517599daf99db47b30dfc9fd9e62d5
Faiz Sulistyawan <faiz.sulistyawan@gmail.com>
2020-09-02 17:43:23 +0700

    auth (temp_done)

---
+++ src/models/auth.js
@@ -0,0 +1,75 @@
+const db = require('../configs/dbMySql')
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken')
+
+
+const authModel = {
+    postNewUser: (body) => {
+        return new Promise((resolve, reject) => {
+            bcrypt.genSalt(10, (err, salt) => {
+                if (!err) {
+                    const { password } = body
+                    bcrypt.hash(password, salt, (err, hashedPassword) => {
+                        if (!err) {
+                            const newBody = { ...body, password: hashedPassword }
+                            const queryString = `SELECT users.username FROM users WHERE username=?`
+                            const secondQS = `INSERT INTO users SET ?`
+                            db.query(queryString, newBody.username, (err, data) => {
+                                if (data.length) {
+                                    reject({ msg: "username udah ada"})
+                                } 
+                                if (!data.length) {
+                                    db.query(secondQS, newBody, (err, result) => {
+                                        if (!err) {
+                                            resolve(result)
+                                        } else {
+                                            reject(err)
+                                        }
+                                    })
+                                }
+                            })
+                        }
+                    })
+                }
+            })
+        })
+    },
+    loginUser: (body) => {
+        return new Promise((resolve, reject) => {
+            const queryString = "SELECT username, password, level_id FROM users WHERE username=?"
+            db.query(queryString, body.username, (err, data) => {
+                if (!err) {
+                    // console.log(data)
+                    if (data.length) {
+                        
+                        bcrypt.compare(body.password, data[0].password, (error, result) => {
+                            if (!result) {
+                                reject({ msg: "wrong password" })
+                            } else if (result === true) {
+                                const { username } = body;
+                                const { level_id } = data[0];
+                                // console.log(body)
+                                const payload = {
+                                    username,
+                                    level_id,
+                                }
+                                const token = jwt.sign(payload, process.env.SECRET_KEY)
+                                const msg = "login success"
+                            
+                                resolve({ msg, token, level_id })
+                            } else {
+                                reject(error)
+                            }
+                        })
+                    } else {
+                        const msg = "username salah"
+                        reject({msg, err})
+                    }
+                } else {
+                    reject(err)
+                }
+            })
+        })
+    }
+}
+module.exports = authModel
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,13 @@ const express = require('express')
 
 
 const authRouter = require('./auth')
-const authRouter = require('./books')
+// const authRouter = require('./books')
 
 
 const indexRouter = express.Router()
 
 
 indexRouter.use('/auth', authRouter)
-indexRouter.use('/books', authRouter)
+// indexRouter.use('/books', authRouter)
 
 module.exports = indexRouter
