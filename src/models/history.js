e122e5d3f1f1177a4cc1cf9b8363340fbe5046f2
Faiz Sulistyawan <faiz.sulistyawan@gmail.com>
2020-09-07 07:23:19 +0700

    done

--- src/models/history.js
+++ src/models/history.js
@@ -3,25 +3,7 @@ const db = require('../configs/dbMySql');
 const historyModels = {
   getAllHistory: (query) => {
     let queryString = "";
-    if (query.title === undefined && query.sortby === undefined && query.order === undefined) {
-      queryString = `SELECT history.id, title, username,users_id, author, borrow_date, duration FROM ((history LEFT JOIN books ON history.books_id=books.id) LEFT JOIN users ON history.users_id = users.id)`;
-    }
-
-    else if (query.sortby === undefined && query.order === undefined) {
-      queryString = `SELECT history.id, title, username,users_id, author, borrow_date, duration FROM ((history LEFT JOIN books ON history.books_id=books.id) LEFT JOIN users ON history.users_id = users.id) WHERE title LIKE '%${query.title}%'`;
-    }
-
-    else if (query.title === undefined) {
-      queryString = `SELECT history.id, title, username,users_id, author, borrow_date, duration FROM ((history LEFT JOIN books ON history.books_id=books.id) LEFT JOIN users ON history.users_id = users.id) ORDER BY ${query.sortby} ${query.order}`;
-    }
-
-    else if (query.title === undefined && query.order===undefined) {
-      queryString = `SELECT history.id, title, username,users_id, author, borrow_date, duration FROM ((history LEFT JOIN books ON history.books_id=books.id) LEFT JOIN users ON history.users_id = users.id) ORDER BY ${query.sortby}`;
-    }
-
-    else {
-      queryString = `SELECT history.id, title, username,users_id, author, borrow_date, duration FROM ((history LEFT JOIN books ON history.books_id=books.id) LEFT JOIN users ON history.users_id = users.id) WHERE title LIKE '%${query.title}%' ORDER BY ${query.sortby} ${query.order}`;
-    }
+    queryString = `SELECT history.id, title, username,users_id, author, borrow_date, duration FROM ((history LEFT JOIN books ON history.books_id=books.id) LEFT JOIN users ON history.users_id = users.id)`;
     return new Promise((resolve, reject) => {
       db.query(queryString, (error, data) => {
         if (!error) {
