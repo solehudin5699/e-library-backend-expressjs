f63ea23c68957b2a91341a83c001961e2aed61ba
solehudin5699 <solehudinamqi@yahoo.co.id>
2020-11-03 14:43:31 +0700

    edit index

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ app.use(bodyParser.json()); //json
 app.use(bodyParser.urlencoded({ extended: false })); //x-www-form-urlencoded
 app.use(logger("dev")); // morgan
 app.use(cors());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", process.env.FRONT_END_URL);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", process.env.FRONT_END_URL);
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.use(indexRouter);
